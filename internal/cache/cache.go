d44e6b0a59a61504a43104ec7f41de4bb72cd5d2
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-07-07 16:32:47 +0300

    stage 6: refactoring try final 1

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -24,18 +24,15 @@ func New() *TTLCache {
 
 func (c *TTLCache) Get(key string) (value any, fetchedAt time.Time, ok bool) {
 	c.mu.RLock()
-	entry := entry{}
-	var is bool
-	if true {
+	entry, is := c.m[key]
+	if !is || entry.expiresAt.After(time.Now()) {
 		defer c.mu.RUnlock()
-		entry, is = c.m[key]
-		if !is || entry.expiresAt.After(time.Now()) {
-			if !is {
-				return nil, time.Time{}, false
-			}
-			return entry.value, entry.fetchedAt, true
+		if !is {
+			return nil, time.Time{}, false
 		}
+		return entry.value, entry.fetchedAt, true
 	}
+	c.mu.RUnlock()
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if entry, is = c.m[key]; is && !entry.expiresAt.After(time.Now()) {
